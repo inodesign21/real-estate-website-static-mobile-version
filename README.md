a04a60f533805038122354ebf168d6a2e1428512
plusownented <plusownented@gmail.com>
2014-10-07 18:13:56 -0300

    commit organizacional
    changes ordem

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 website-static-mobile-version
 =============================
 
-client real estate static website
+client real estate static website - by inO Design (inodesign.com.br)
 
 demo: https://www.googledrive.com/host/0B1tEj1IOkWCMblA3NTRfcTlOZzg
--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
+// Accordion Toggle Icon
+          function toggleChevron(e) {
+              $(e.target)
+                  .prev('.panel-heading')
+                  .find("i.indicator")
+                  .toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+          }
+          $('#accordion').on('hidden.bs.collapse', toggleChevron);
+          $('#accordion').on('shown.bs.collapse', toggleChevron);
 
+
+// Select Pretensão -->
+        $(document).ready(function() {
+            $('#pretensao').multiselect({
+                nonSelectedText: 'Pretensão'
+            });
+        });
+
+
+// Select Tipo
+        $(document).ready(function() {
+            $('#tipo').multiselect({
+                nonSelectedText: 'Tipo'
+            });
+        });
+
+
+// Valor
+        $(document).ready(function() {
+            $('#valor').multiselect({
+                nonSelectedText: 'Valor'
+            });
+        });
+
+
+// Ajustes do Multiseleção do Bairro
+        $(document).ready(function() {
+            $('#bairro').multiselect({
+                numberDisplayed: 2,
+                maxHeight: 250,
+                nonSelectedText: 'Bairro',
+                enableFiltering: true,
+                filterBehavior: 'both',
+                enableCaseInsensitiveFiltering: false,
+                filterPlaceholder: 'Procure',
+                includeSelectAllOption: true,
+                selectAllText: 'Todos',
+                nSelectedText: 'bairros'
+            });
+        });
